a95ff5750ac973c08157b86f589d78b63c5e0d76
Cyril <cyril.bier42100@gmail.com>
2024-10-18 15:47:45 +0200

    Init project

---
+++ footballPOC/src/main/java/com/cbi/footballPOC/FootballPocApplication.java
@@ -0,0 +1,13 @@
+package com.cbi.footballPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPocApplication.class, args);
+	}
+
+}
---
+++ footballPOC/src/test/java/com/cbi/footballPOC/FootballPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbi.footballPOC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
